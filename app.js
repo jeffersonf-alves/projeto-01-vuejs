fb464bc0365944f0a2986e8b1aa9f0d9d90cf421
Jefferson Alves <jefff9910@outlook.com>
2022-01-05 23:52:17 -0300

    Acoes dos botoes atacar e ataque especial

--- app.js
+++ app.js
@@ -15,9 +15,25 @@ new Vue({
             this.running = true
             this.playerLife = 100
             this.monsterLife = 100
+        },
+        attack(especial) {
+            this.hurt('monsterLife',5, 10, false)
+            this.hurt('playerLife',7, 12, false)
+        },
+        hurt(pro, min, max, especial) {
+            const plus = especial ? 5 : 0
+            const hurt = this.getRandom(min + plus, max + plus)
+            this[pro] = Math.max(this[pro] - hurt, 0)
+        },
+        getRandom(min, max) {
+            const value = Math.random() * (max - min) + min
+            return Math.round(value)
         }
+    
     }, 
     watch : {
-        
+        hasResult(value) {
+            if (value) this.running = false
+        }
     }
 })
